1c4ce8f29890f612401161fc96e2a9cc9bac061d
Fei Sun <feisun@fb.com>
2018-01-05 15:14:33 -0800

    Restructure git_driver. Now the benchmark run and caffe2 build are in two separate threads. The build thread keeps building Caffe2. After one build is done, the binary is pushed to a queue. The benchmark run thread pops from the queue and kicks off the android benchmark runs. The built binary is saved in hard drive. If the benchmark needs to re-run on a specific command and the binary is already built before, the binary is re-used. This way, we can re-run previous commits easily and more consistently. It is needed to confirm a detected performance regression.

--- git_driver.py
+++ git_driver.py
@@ -8,17 +8,23 @@
 # LICENSE file in the root directory of this source tree.
 ##############################################################################
 
+from collections import deque
 import datetime
 import json
 import os
 import shutil
 import tempfile
+import threading
 import time
 from utils.arg_parse import getParser, getArgs, getUnknowns, parseKnown
 from utils.git import Git
 from utils.custom_logger import getLogger
 from utils.subprocess_with_logger import processRun
 
+getParser().add_argument("--exec_dir", required=True,
+    help="The executable is saved in the specified directory. " +
+    "If an executable is found for a commit, no re-compilation is performed. " +
+    "Instead, the previous compiled executable is reused.")
 getParser().add_argument("--config", required=True,
     help="Required. The test config file containing all the tests to run")
 getParser().add_argument("--git_base_commit",
@@ -52,77 +58,78 @@ platforms.add_argument("--host", action="store_true",
 platforms.add_argument("--android", action="store_true",
     help="Run the benchmark on all connected android devices.")
 
-class GitDriver(object):
-    def __init__(self):
-        parseKnown()
-        self.git = Git(getArgs().git_dir)
-        self.control_commit_hash = None
-        self.prev_commit_hash = None
-        self.git_info = {}
 
-    def runOnce(self, tempdir):
-        getLogger().info(
-            "Start test run @ %s" % datetime.datetime.now().isoformat(' '))
-        if not self._pullNewCommits():
-            return False
-        temptempdir = tempdir + "/temp/"
-        os.mkdir(temptempdir)
-        git_info = self._setupGit(tempdir)
-        if git_info != None:
-            configs = self._processConfig(git_info)
-            for config in configs:
-                cmds = config.split(' ')
-                cmd = [x.strip() for x in cmds]
-                cmd.append("--temp_dir")
-                cmd.append(temptempdir)
-                getLogger().info("Running: %s", ' '.join(cmd))
-                # cannot use subprocess because it conflicts with requests
-                os.system(' '.join(cmd))
-        else:
-            getLogger().error("Failed to extract git commands. Skip this commit.")
-        shutil.rmtree(temptempdir, True)
-        if getArgs().git_commit_file:
-            with open(getArgs().git_commit_file, 'w') as file:
-                file.write(self.prev_commit_hash)
-        getLogger().info("Done one benchmark run.")
-        return True
+def stopRun():
+    if getArgs().status_file and os.path.isfile(getArgs().status_file):
+        with open(getArgs().status_file, 'r') as file:
+            content = file.read().strip()
+            if content == "0":
+                return True
+    return False
+
+
+class ExecutablesBuilder (threading.Thread):
+    def __init__(self, work_queue, queue_lock):
+        threading.Thread.__init__(self)
+        self.git = Git(getArgs().git_dir)
+        self.work_queue = work_queue
+        self.queue_lock = queue_lock
+        self.current_commit_hash = None
 
     def run(self):
-        tempdir = tempfile.mkdtemp()
-        if not getArgs().interval:
-            getLogger().info("Single run...")
-            self.runOnce(tempdir)
-            return
-        getLogger().info("Continuous run...")
-        interval = getArgs().interval
-        while True:
-            if getArgs().status_file and os.path.isfile(getArgs().status_file):
-                with open(getArgs().status_file, 'r') as file:
-                    content = file.read().strip()
-                    if content == "0":
-                        shutil.rmtree(tempdir, True)
-                        getLogger().info("Exiting...")
-                        return
-            prev_ts = time.time()
-            if not self.runOnce(tempdir):
-                current_ts = time.time()
-                if current_ts < prev_ts + interval:
-                    time.sleep(prev_ts + interval - current_ts)
+        if getArgs().interval:
+            while not stopRun():
+                self._buildExecutables()
+                time.sleep(getArgs().interval)
+        else:
+            # single run
+            self._buildExecutables()
+
+    def _buildExecutables(self):
+        while not stopRun() and self._pullNewCommits():
+            git_info = self._buildOneCommitExecutable(self.current_commit_hash)
+            if git_info is None:
+                getLogger().error(
+                    "Failed to extract git commands. Skip this commit.")
+            else:
+                self.queue_lock.acquire()
+                self.work_queue.append(git_info)
+                self.queue_lock.release()
+
+    def _buildOneCommitExecutable(self, commit_hash):
+        git_info = {}
+        git_info_treatment = self._setupGitStep(commit_hash)
+        if git_info_treatment is None:
+            return None
+        git_info['treatment'] = git_info_treatment
+        # figure out the base commit. It is the first commit in the week
+        control_commit_hash = self._getControlCommit(
+            git_info_treatment['commit_time'], getArgs().git_base_commit)
+
+        git_info_control = self._setupGitStep(control_commit_hash)
+        if git_info_control is None:
+            return None
+        git_info['control'] = git_info_control
+        return git_info
 
     def _pullNewCommits(self):
+        # first get into the correct branch
+        self.git.checkout(getArgs().git_branch)
         self.git.pull(getArgs().git_repository, getArgs().git_branch)
         new_commit_hash = None
-        if self.prev_commit_hash == None:
-            self.prev_commit_hash = self._getSavedCommit()
+        if self.current_commit_hash is None:
+            self.current_commit_hash = self._getSavedCommit()
 
-        if self.prev_commit_hash == None:
+        if self.current_commit_hash is None or not getArgs().interval:
             new_commit_hash = self.git.getCommitHash(getArgs().git_commit)
         else:
-            new_commit_hash = self.git.getNextCommitHash(self.prev_commit_hash)
-        if new_commit_hash == self.prev_commit_hash:
-            getLogger().info("Commit %s is already processed, sleeping...", new_commit_hash)
+            new_commit_hash = self.git.getNextCommitHash(
+                self.current_commit_hash)
+        if new_commit_hash == self.current_commit_hash:
+            getLogger().info("Commit %s is already processed, sleeping...",
+                             new_commit_hash)
             return False
-        self.prev_commit_hash = new_commit_hash
+        self.current_commit_hash = new_commit_hash
         return True
 
     def _getSavedCommit(self):
@@ -131,109 +138,76 @@ class GitDriver(object):
             with open(getArgs().git_commit_file, 'r') as file:
                 commit_hash = file.read().strip()
                 # verify that the commit exists
-                commits = self.git.run('rev-list', '--reverse', commit_hash+"^..HEAD").strip().split('\n')
+                commits = self.git.run('rev-list',
+                                       '--reverse',
+                                       commit_hash+"^..HEAD").\
+                    strip().split('\n')
                 if commits[0] == commit_hash:
                     return commit_hash
         return None
 
-
-    def _setupGit(self, tempdir):
-        git_info = {}
-        treatment_dir = tempdir + '/treatment'
-        if os.path.isdir(treatment_dir):
-            shutil.rmtree(treatment_dir, True)
-        os.mkdir(treatment_dir)
-        git_info_treatment = self._setupGitStep(treatment_dir, self.prev_commit_hash, False)
-        if git_info_treatment is None:
-            return None
-        git_info['treatment'] = git_info_treatment
-        # figure out the base commit. It is the first commit in the week
-        control_commit_hash = self._getControlCommit(git_info_treatment['commit_time'], getArgs().git_base_commit)
-        # control may not change that frequent
-        control_dir = tempdir + '/control'
-        if not os.path.isdir(control_dir):
-            os.mkdir(control_dir)
-
-        git_info_control = self._setupGitStep(control_dir, control_commit_hash,
-            control_commit_hash == self.control_commit_hash)
-        if git_info_control is None:
-            return None
-        git_info['control'] = git_info_control
-        if control_commit_hash == self.control_commit_hash:
-            getLogger().info("Program for control commit %s has already built" % control_commit_hash)
-        self.control_commit_hash = control_commit_hash
-
-        # restore the original commit
-        self.git.checkout(getArgs().git_branch)
-        return git_info
-
-    def _setupGitStep(self, tempdir, commit, skip_build):
+    def _setupGitStep(self, commit):
         git_info = {}
         git_info['commit'] = self.git.getCommitHash(commit)
         git_info['commit_time'] = self.git.getCommitTime(git_info['commit'])
-        if not skip_build:
-            self.git.checkout(commit)
-        return git_info if self._buildProgram(tempdir, git_info, skip_build) else None
+        return git_info if self._buildProgram(git_info) else None
 
-    def _buildProgram(self, tempdir, git_info, skip_build):
+    def _buildProgram(self, git_info):
+        dt = datetime.datetime.fromtimestamp(
+            git_info['commit_time'], datetime.timezone.utc)
+        directory = getArgs().exec_dir + "/" + \
+            str(dt.year) + "/" + \
+            str(dt.month) + "/" + \
+            str(dt.day) + "/" + \
+            git_info['commit'] + "/"
+
         if getArgs().android:
             src = getArgs().git_dir + \
                 '/build_android/bin/caffe2_benchmark'
-            dst = tempdir + '/caffe2_benchmark_android'
+            dst = directory + '/android/caffe2_benchmark'
             build_dir = getArgs().git_dir + "/build_android"
-            script = getArgs().git_dir + "/scripts/build_android.sh -DBUILD_BINARY=ON -DBUILD_SHARE_DIR=ON -DUSE_ZSTD=ON"
+            script = getArgs().git_dir + \
+                "/scripts/build_android.sh -DBUILD_BINARY=ON " + \
+                "-DBUILD_SHARE_DIR=ON -DUSE_ZSTD=ON"
         elif getArgs().host:
             src = getArgs().git_dir + \
                 '/build/bin/caffe2_benchmark'
-            dst = tempdir + '/caffe2_benchmark_host'
+            dst = directory + '/host/caffe2_benchmark'
             build_dir = getArgs().git_dir + "/build"
-            script = getArgs().git_dir + "/scripts/build_local.sh -DBUILD_BINARY=ON -DBUILD_SHARE_DIR=ON -DUSE_ZSTD=ON"
+            script = getArgs().git_dir + \
+                "/scripts/build_local.sh -DBUILD_BINARY=ON " + \
+                "-DBUILD_SHARE_DIR=ON -DUSE_ZSTD=ON"
         else:
             getLogger().error("At least one platform needs to be specified.")
-        return self._buildProgramPlatform(git_info, src, dst, build_dir, script, skip_build)
-
-    def _buildProgramPlatform(self, git_info, src, dst, build_dir, script, skip_build):
-        if not skip_build:
-            shutil.rmtree(build_dir, True)
-            if processRun(script.split(' ')) != None:
-                shutil.copyfile(src, dst)
-                os.chmod(dst, 0o777)
+        git_info["program"] = dst
         if os.path.isfile(dst):
-            git_info["program"] = dst
+            return True
         else:
-            if skip_build:
-                getLogger().error("The build is skipped, but the file " + \
-                    "%s doesn't exist." % dst)
-            else:
-                getLogger().error("Build Caffe2 failed.")
+            return self._buildProgramPlatform(
+                git_info, src, dst, build_dir, script)
+
+    def _buildProgramPlatform(self, git_info, src, dst, build_dir, script):
+        self.git.checkout(git_info['commit'])
+        shutil.rmtree(build_dir, True)
+        if processRun(script.split(' ')) is not None:
+            directory = os.path.dirname(dst)
+            if not os.path.isdir(directory):
+                os.makedirs(directory)
+            shutil.copyfile(src, dst)
+            os.chmod(dst, 0o777)
+
+        if not os.path.isfile(dst):
+            getLogger().error("Build Caffe2 failed.")
             return False
         return True
-
-    def _processConfig(self, git_info):
-        dir_path = os.path.dirname(os.path.realpath(__file__))
-        models_dir = getArgs().models_dir
-        unknowns = getUnknowns()
-        with open(getArgs().config, 'r') as file:
-            content = json.load(file)
-            assert content["tests"], "Test is not specified in the config file"
-            configs = [dir_path +
-                "/harness.py " +
-                x["args"].strip().replace('<models_dir>', models_dir) +
-                ((" --excluded_platforms \"" + x["excluded_platforms"] + "\"") \
-                    if x["excluded_platforms"] else "") +
-                (" --android" if getArgs().android else "") +
-                (" --host" if getArgs().host else "") +
-                (" --info \'" + json.dumps(git_info) + "\'") + " " +
-                ' '.join(['"' + u + '"' for u in unknowns])
-                for x in content["tests"]]
-        return configs
 
     def _getControlCommit(self, reference_time, base_commit):
         # Get start of week
         dt = datetime.datetime.fromtimestamp(reference_time,
-            datetime.timezone.utc)
+                                             datetime.timezone.utc)
         monday = dt - datetime.timedelta(days=dt.weekday())
-        start_of_week = monday.replace(hour=0, minute=0, second=0, microsecond=0)
+        start_of_week = monday.replace(hour=0, minute=0,
+                                       second=0, microsecond=0)
         ut_start_of_week = start_of_week.timestamp()
 
         if base_commit:
@@ -244,8 +218,13 @@ class GitDriver(object):
         # Give more buffer to the date range to avoid the timezone issues
         start = start_of_week - datetime.timedelta(days=1)
         end = dt + datetime.timedelta(days=1)
-        logs_str = self.git.run('log', '--after', start.isoformat(), '--before',
-            end.isoformat(), '--reverse', '--pretty=format:%H:%ct').strip()
+        logs_str = self.git.run('log',
+                                '--after',
+                                start.isoformat(),
+                                '--before',
+                                end.isoformat(),
+                                '--reverse',
+                                '--pretty=format:%H:%ct').strip()
         logs = logs_str.split('\n')
         for row in logs:
             items = row.strip().split(':')
@@ -256,6 +235,83 @@ class GitDriver(object):
                 return commit_hash
         assert False, "Cannot find the control commit"
         return None
+
+
+class GitDriver(object):
+    def __init__(self):
+        parseKnown()
+        self.queue_lock = threading.Lock()
+        self.work_queue = deque()
+        self.executables_builder = ExecutablesBuilder(self.work_queue,
+                                                      self.queue_lock)
+
+    def run(self):
+        getLogger().info(
+            "Start benchmark run @ %s" %
+            datetime.datetime.now().isoformat(' '))
+        self.executables_builder.start()
+        self._runBenchmarkSuites()
+
+    def _runBenchmarkSuites(self):
+        # initially sleep 10 seconds in case no need to build the binary
+        time.sleep(10)
+        if getArgs().interval:
+            while not stopRun():
+                self._runBenchmarkSuitesInQueue()
+                time.sleep(getArgs().interval)
+        else:
+            # single run
+            while self.executables_builder.is_alive():
+                time.sleep(10)
+            self._runBenchmarkSuitesInQueue()
+
+    def _runBenchmarkSuitesInQueue(self):
+        tempdir = tempfile.mkdtemp()
+        while not stopRun() and self.work_queue:
+            # we can do this because this is the only
+            # consumer of the work_queue
+            self.queue_lock.acquire()
+            git_info = self.work_queue.popleft()
+            self.queue_lock.release()
+            self._runOneBenchmarkSuite(git_info, tempdir)
+        shutil.rmtree(tempdir, True)
+
+    def _runOneBenchmarkSuite(self, git_info, tempdir):
+        configs = self._processConfig(git_info)
+        for config in configs:
+            cmds = config.split(' ')
+            cmd = [x.strip() for x in cmds]
+            cmd.append("--temp_dir")
+            cmd.append(tempdir)
+            getLogger().info("Running: %s", ' '.join(cmd))
+            # cannot use subprocess because it conflicts with requests
+            os.system(' '.join(cmd))
+        if getArgs().git_commit_file:
+            with open(getArgs().git_commit_file, 'w') as file:
+                file.write(git_info['treatment']['commit'])
+        getLogger().info("Done one benchmark run for " +
+                         git_info['treatment']['commit'])
+
+    def _processConfig(self, git_info):
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        models_dir = getArgs().models_dir
+        unknowns = getUnknowns()
+        with open(getArgs().config, 'r') as file:
+            content = json.load(file)
+            assert content["tests"], "Test is not specified in the config file"
+            configs = [dir_path +
+                       "/harness.py " +
+                       x["args"].strip().replace('<models_dir>', models_dir) +
+                       ((" --excluded_platforms \"" +
+                        x["excluded_platforms"] + "\"")
+                        if x["excluded_platforms"] else "") +
+                       (" --android" if getArgs().android else "") +
+                       (" --host" if getArgs().host else "") +
+                       (" --info \'" + json.dumps(git_info) + "\'") + " " +
+                       ' '.join(['"' + u + '"' for u in unknowns])
+                       for x in content["tests"]]
+        return configs
+
 
 if __name__ == "__main__":
     app = GitDriver()
