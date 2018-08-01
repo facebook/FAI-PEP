a87c08f44856464091759e93ce86a4747ff404f4
Fei Sun <feisun@fb.com>
2018-07-31 17:50:18 -0700

    TFLite reports operator statistics

--- benchmarking/frameworks/tflite/tflite.py
+++ benchmarking/frameworks/tflite/tflite.py
@@ -82,63 +82,102 @@ class TFLiteFramework(FrameworkBase):
         output = platform.runBenchmark(cmd, platform_args=platform_args,
                                        log_to_screen_only=True)
         result = self._collectData(output)
-        metric = self._processData(result)
-        return metric
+        return result
 
     def _collectData(self, output):
         if output is None:
             return False
-        result = {}
+        results = {}
         rows = output.split('\n')
         # only collect one data point for statistics
         # the actual run data should override the warmup data
         i = 0
         while i < len(rows):
-            row = rows[i]
-            if row[0:21] == "Running benchmark for":
-                assert len(rows) > i+1, "Valid row cannot be found"
-                data = rows[i+1]
-                pattern = re.compile(r"^count=([\d|\.]+) first=([\d|\.]+) curr=([\d|\.]+) min=([\d|\.]+) max=([\d|\.]+) avg=([\d|\.]+) std=([\d|\.]+)")
-                match = pattern.match(data)
-                if match:
-                    result["NET"] = {
-                        "count": int(match.group(1)),
-                        "min": float(match.group(4)),
-                        "max": float(match.group(5)),
-                        "avg": float(match.group(6)),
-                        "std": float(match.group(7))
-                    }
-                else:
-                    pattern = re.compile(r"^count=(\d+) curr=(\d+)")
-                    match = pattern.match(data)
-                    assert match, \
-                        "No data is collected for {}".format(data)
-
-                    result["NET"] = {
-                        "count": int(match.group(1)),
-                        "min": float(match.group(2)),
-                        "max": float(match.group(2)),
-                        "avg": float(match.group(2)),
-                        "std": 0
-                    }
+            i = self._collectNETLatency(results, rows, i)
+            i = self._collectOperatorLatency(results, rows, i)
             i += 1
-        return result
-
-    def _processData(self, data):
-        assert len(data) == 1, "Only one line should be collected"
-        result = {
-            "type": "NET",
-            "unit": "us",
-            "metric": "latency",
-            "num_runs": data["NET"]["count"],
-            "summary": {
-                "p0": data["NET"]["min"],
-                "p100": data["NET"]["max"],
-                "mean": data["NET"]["avg"],
-                "stdev": data["NET"]["std"],
-            }
-        }
-        results = {
-            "NET latency": result
-        }
         return results
+
+    def _collectNETLatency(self, results, rows, i):
+        row = rows[i]
+        if row[:21] == "Running benchmark for":
+            assert len(rows) > i+1, "Valid row cannot be found"
+            i = i + 1
+            data = rows[i]
+            pattern = re.compile(r"^count=([\d|\.]+) first=([\d|\.]+) curr=([\d|\.]+) min=([\d|\.]+) max=([\d|\.]+) avg=([\d|\.]+) std=([\d|\.]+)")
+            match = pattern.match(data)
+            if match:
+                r = {
+                    "count": int(match.group(1)),
+                    "min": float(match.group(4)),
+                    "max": float(match.group(5)),
+                    "avg": float(match.group(6)),
+                    "std": float(match.group(7))
+                }
+            else:
+                pattern = re.compile(r"^count=(\d+) curr=(\d+)")
+                match = pattern.match(data)
+                assert match, \
+                    "No data is collected for {}".format(data)
+
+                r = {
+                    "count": int(match.group(1)),
+                    "min": float(match.group(2)),
+                    "max": float(match.group(2)),
+                    "avg": float(match.group(2)),
+                    "std": 0
+                }
+            results["NET latency"] = {
+                "type": "NET",
+                "unit": "us",
+                "metric": "latency",
+                "num_runs": r["count"],
+                "summary": {
+                    "p0": r["min"],
+                    "p100": r["max"],
+                    "mean": r["avg"],
+                    "stdev": r["std"],
+                }
+            }
+            i = i + 1
+        return i
+
+    def _collectOperatorLatency(self, results, rows, i):
+        row = rows[i]
+        if row[:71] == "============================== Run Order ==============================":
+            i = i + 2
+            types_table = {}
+            pattern = re.compile(r"\s+(\w+)\s+([\d|\.]+)\s+([\d|\.]+)\s+([\d|\.]+)\s+([\d|\.]+)%\s+([\d|\.]+)%\s+([\d|\.]+)\s+([\d|\.]+)\s+\[(.+)\]")
+            while i < len(rows):
+                row = rows[i]
+                match = pattern.match(row)
+                if not match:
+                    break
+                type = match.group(9)
+                kind = match.group(1)
+                avg = float(match.group(4)) * 1000
+                results[type + " latency"] = {
+                    "type": type,
+                    "unit": "us",
+                    "metric": "latency",
+                    "summary": {
+                        "mean": avg
+                    }
+                }
+                if kind in types_table:
+                    types_table[kind] += avg
+                else:
+                    types_table[kind] = avg
+                i = i + 1
+            # Write the accumulated operator types
+            for k in types_table:
+                v = types_table[k]
+                results[k + " latency"] = {
+                    "type": k,
+                    "unit": "us",
+                    "metric": "latency",
+                    "summary": {
+                        "mean": v
+                    }
+                }
+        return i
