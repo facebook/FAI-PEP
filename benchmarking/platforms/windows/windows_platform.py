2751826f37a4a153678678542bc019e00bf53823
Fei Sun <feisun@fb.com>
2018-08-15 20:37:25 -0700

    Remove unused imports

--- benchmarking/platforms/windows/windows_platform.py
+++ benchmarking/platforms/windows/windows_platform.py
@@ -8,20 +8,8 @@
 # LICENSE file in the root directory of this source tree.
 ##############################################################################
 
-import platform
-import os
-import random
-import re
-import shlex
-import shutil
-import socket
-import subprocess
 
 from platforms.host.host_platform import HostPlatform
-from six import string_types
-from utils.custom_logger import getLogger
-from utils.arg_parse import getArgs
-from utils.subprocess_with_logger import processRun
 
 
 class WindowsPlatform(HostPlatform):
