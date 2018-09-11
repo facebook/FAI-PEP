bfc027afd69a5c74afdd6de0d4217597960be433
sf-wind <sf_wind@hotmail.com>
2018-09-11 14:53:22 -0700

    Add incremental host build for caffe2 (#94)

---
+++ specifications/frameworks/caffe2/host/incremental/build.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+$1/scripts/build_local.sh -DBUILD_SHARED_LIBS=OFF -DBUILD_BINARY=ON -DBUILD_SHARE_DIR=ON -DBUILD_OBSERVERS=ON -DUSE_ZSTD=ON -DUSE_OBSERVERS=ON
+cp $1/build/bin/caffe2_benchmark $2
