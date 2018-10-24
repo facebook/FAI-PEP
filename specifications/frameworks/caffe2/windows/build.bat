2964e9c70fd2f9f12130df8a4ccf92ffea8adaed
sf-wind <sf_wind@hotmail.com>
2018-10-24 11:59:44 -0700

    Enable build binary and support new CUDA versions (#129)
    * Support windows CUDA build
    
    * Minor changes to internal flow to reflect the new CUDA version

--- specifications/frameworks/caffe2/windows/build.bat
+++ specifications/frameworks/caffe2/windows/build.bat
@@ -1,6 +1,7 @@
 setlocal
+rd /Q /S %1\build
 SET USE_OBSERVERS=ON
-del %1\build
+SET BUILD_BINARY=ON
 call %1\scripts\build_windows.bat || exit /b 1
 copy %1\build\bin\Release\caffe2_benchmark.exe %2
 endlocal
