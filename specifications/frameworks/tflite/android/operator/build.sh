837dc45bdb8413d69d5ff404aeeb74150bd16b3c
sf-wind <sf_wind@hotmail.com>
2018-11-02 21:22:33 -0700

    For tflite, no need to push/pop directory (#138)

--- specifications/frameworks/tflite/android/operator/build.sh
+++ specifications/frameworks/tflite/android/operator/build.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 # the configure must be run once beforehand
-pushd `pwd`
 cd "$1"
 
 # build benchmark binary
@@ -11,7 +10,5 @@ bazel build -c opt \
   --cxxopt='--std=c++11' \
   --copt=-DTFLITE_PROFILING_ENABLED \
   tensorflow/contrib/lite/tools/benchmark:benchmark_model
-
-popd
 
 cp "$1/bazel-bin/tensorflow/contrib/lite/tools/benchmark/benchmark_model" "$2"
--- specifications/frameworks/tflite/host/build.sh
+++ specifications/frameworks/tflite/host/build.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
 
 # the configure must be run once beforehand
-pushd `pwd`
 cd "$1"
 
 # build benchmark binary
 # assuming the configure has set up the NDK and SDK correctly
 bazel build -c opt \
   tensorflow/contrib/lite/tools/benchmark:benchmark_model
-
-popd
 
 cp "$1/bazel-bin/tensorflow/contrib/lite/tools/benchmark/benchmark_model" "$2"
