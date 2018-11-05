e1326c18dc1970e90b077ff3ddcd25b0c4a51896
sf-wind <sf_wind@hotmail.com>
2018-11-05 11:20:11 -0800

    Add set -ex to all shell script (#141)

--- specifications/frameworks/caffe2/android/build.sh
+++ specifications/frameworks/caffe2/android/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -ex
+
 rm -rf $1/build*
 $1/scripts/build_android.sh -DBUILD_BINARY=ON -DUSE_OBSERVERS=ON -DUSE_ZSTD=ON -DBUILD_SHARE_DIR=ON -DBUILD_OBSERVERS=ON
 cp $1/build_android/bin/caffe2_benchmark $2
