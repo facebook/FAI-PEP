6753e88e17dd3c7c8bc04e6cce09f13392a03bc2
Fei Sun <feisun@fb.com>
2018-11-06 18:00:26 -0800

    Also copy convert_image_to_tensor to target directory in host build

--- specifications/frameworks/caffe2/host/build.sh
+++ specifications/frameworks/caffe2/host/build.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 set -ex
+DIR=$(dirname "$2")
 
 rm -rf $1/build/
 $1/scripts/build_local.sh -DBUILD_SHARED_LIBS=OFF -DBUILD_BINARY=ON -DBUILD_SHARE_DIR=ON -DBUILD_OBSERVERS=ON -DUSE_ZSTD=ON -DUSE_OBSERVERS=ON
 cp $1/build/bin/caffe2_benchmark $2
+cp "$1/build/bin/convert_image_to_tensor" "${DIR}/convert_image_to_tensor"
--- specifications/frameworks/caffe2/host/incremental/build.sh
+++ specifications/frameworks/caffe2/host/incremental/build.sh
@@ -2,5 +2,8 @@
 
 set -ex
 
+DIR=$(dirname "$2")
+
 $1/scripts/build_local.sh -DBUILD_SHARED_LIBS=OFF -DBUILD_BINARY=ON -DBUILD_SHARE_DIR=ON -DBUILD_OBSERVERS=ON -DUSE_ZSTD=ON -DUSE_OBSERVERS=ON
 cp $1/build/bin/caffe2_benchmark $2
+cp "$1/build/bin/convert_image_to_tensor" "${DIR}/convert_image_to_tensor"
