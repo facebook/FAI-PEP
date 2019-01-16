e6f40d9c7ca4fbb22d8f66bfcc5e410e229d7473
Fei Sun <feisun@fb.com>
2019-01-16 14:58:36 -0800

    Add imagenet test to CI

--- .circleci/builds/build_pytorch.sh
+++ .circleci/builds/build_pytorch.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-pip install numpy pyyaml mkl mkl-include setuptools cmake cffi typing
+pip install numpy pyyaml mkl mkl-include setuptools cmake cffi typing wget
 
 REPO_DIR=/tmp/pytorch
 
@@ -11,3 +11,12 @@ git clone --recursive --quiet https://github.com/pytorch/pytorch.git "$REPO_DIR"
 
 # install ninja to speedup the build
 pip install ninja
+
+wget -O /tmp/opencv.zip https://github.com/opencv/opencv/archive/3.4.3.zip
+unzip -q /tmp/opencv.zip -d /tmp/
+cd /tmp/opencv-3.4.3/
+mkdir build
+cd build
+cmake ..
+make -j 1
+make install
