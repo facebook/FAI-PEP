beaa750ce212d1e45c60237100113c959e01c4f2
Huamin Li <huaminli@fb.com>
2019-01-29 00:17:11 -0800

    fix tflite

--- benchmarking/reporters/screen_reporter/screen_reporter.py
+++ benchmarking/reporters/screen_reporter/screen_reporter.py
@@ -12,7 +12,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
-import cocy
+import copy
 import datetime
 
 from reporters.reporter_base import ReporterBase
