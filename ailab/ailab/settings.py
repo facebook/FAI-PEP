14f3c2a2452d66a1ad3c78441b0542757261ecf6
Zhizhen Qin <zhizhenqin@fb.com>
2019-02-20 11:31:56 -0800

    Add allowed hosts

--- ailab/ailab/settings.py
+++ ailab/ailab/settings.py
@@ -33,7 +33,7 @@ except ImportError:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
