17f64f6f01b6af09ce4382b40dda505b57a4bfc9
Zhizhen Qin <zhizhenqin@fb.com>
2019-03-17 10:02:58 -0700

    Add bench mark result table to OSS as remote reporter
    Differential Revision: D14425510
    
    fbshipit-source-id: a99064330498cd87a6f1fa91062ac182f6cd41ea

--- ailab/benchmark/views.py
+++ ailab/benchmark/views.py
@@ -1,15 +1,26 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import json
+
 from django.http import JsonResponse
 
 from django.views.decorators.csrf import csrf_exempt
 import db_controller
+import benchmark_result_controller
 
 
 @csrf_exempt
 def handle_request(request):
     data = request.POST.copy()
     results = db_controller.get_payload(data)
+
+    return JsonResponse(results)
+
+
+@csrf_exempt
+def store_benchmark_result(request):
+    data = json.loads(request.body)
+    results = benchmark_result_controller.store_result(data)
 
     return JsonResponse(results)
