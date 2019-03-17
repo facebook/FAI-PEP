7f5d6722440ec82a570870bfb5f6d1b8a8881e66
Zhizhen Qin <zhizhenqin@fb.com>
2019-03-17 15:52:22 -0700

    Add bench mark result table to OSS as remote reporter (#242)
    Summary:
    Pull Request resolved: https://github.com/facebook/FAI-PEP/pull/242
    
    This diff creates benchmark result table and its corresponding handler in OSS and allow remote reporter to use.
    
    Reviewed By: sf-wind
    
    Differential Revision: D14425510
    
    fbshipit-source-id: ae33def9d16f22ac4e73fba97e3e8270ae6d210d

---
+++ ailab/benchmark/benchmark_result_controller.py
@@ -0,0 +1,46 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import json
+
+from .models import BenchmarkResult
+
+
+def store_result(data):
+    logs = json.loads(data['logs'])
+
+    for log in logs:
+        store_single_entry(log)
+
+    return {"status": "success"}
+
+
+def store_single_entry(log):
+    message = json.loads(log['message'])
+
+    r = BenchmarkResult()
+
+    process_entry(r, message, 'int')
+    process_entry(r, message, 'normal')
+    process_vec_entry(r, message, 'normvector')
+
+    r.save()
+
+
+def process_entry(r, message, type):
+    for k, v in message[type].items():
+        if k == 'net_name':
+            k = 'model'
+        try:
+            setattr(r, k, v)
+        except Exception:
+            print("{} is not a valid field")
+
+def process_vec_entry(r, message, type):
+    for k, v in message[type].items():
+        if k == 'net_name':
+            k = 'model'
+        v = json.dumps(v)
+        try:
+            setattr(r, k, v)
+        except Exception:
+            print("{} is not a valid field")
--- ailab/benchmark/urls.py
+++ ailab/benchmark/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.handle_request, name='index'),
+    url(r'^store-result$', views.store_benchmark_result, name='store_benchmark_result'),
 ]
