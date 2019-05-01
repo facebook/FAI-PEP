ef2d05399f025f8bb945cb983d33bc84431ba16b
Zhizhen Qin <eliqinzz@gmail.com>
2019-05-01 10:43:22 -0700

    Fix code formats

--- ailab/ailab/views.py
+++ ailab/ailab/views.py
@@ -1,4 +1,6 @@
+from future import absolute_import, division, print_function, unicode_literals
 from django.shortcuts import redirect
+
 
 def redirect_to_viz(request):
     response = redirect('/benchmark/visualize')
