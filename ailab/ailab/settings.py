def7cb35688f616bded5ebb75edbdf5ed00a27f7
Fei Sun <feisun@fb.com>
2019-05-01 16:41:19 -0700

    Cannot change debug to be true

--- ailab/ailab/settings.py
+++ ailab/ailab/settings.py
@@ -32,7 +32,7 @@ except ImportError:
     from .secret_key import SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
