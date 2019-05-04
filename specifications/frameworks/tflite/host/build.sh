c0ddfaecdf1bda3dab1718a03f9c55d13c0df237
Fei Sun <feisun@fb.com>
2019-05-04 12:35:51 -0700

    fix lint error

--- specifications/frameworks/tflite/host/build.sh
+++ specifications/frameworks/tflite/host/build.sh
@@ -5,17 +5,17 @@ set -ex
 DIR=${PWD}
 # the configure must be run once beforehand
 cd "$1"
-unamestr=`uname`
-echo "OS is $unamestr"
+
+echo "OS is ${uname}"
 
 LINKER_OPTS=""
-if [ "$unamestr" = 'Linux' ]; then
+if [ "${uname}" = 'Linux' ]; then
   LINKER_OPTS="--linkopt -latomic"
 fi
 echo "OPTS: ${LINKER_OPTS}"
 # build benchmark binary
 # assuming the configure has set up the NDK and SDK correctly
-bazel build -c opt ${LINKER_OPTS} \
+bazel build -c opt "${LINKER_OPTS}" \
   tensorflow/lite/tools/benchmark:benchmark_model
 
 cd ${DIR}
