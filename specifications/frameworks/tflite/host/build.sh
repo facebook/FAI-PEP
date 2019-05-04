32391cada3b31f5417fbc9f4a0157836e738975a
Fei Sun <feisun@fb.com>
2019-05-03 18:07:46 -0700

    conditionalize to only add linker flags on linux

--- specifications/frameworks/tflite/host/build.sh
+++ specifications/frameworks/tflite/host/build.sh
@@ -4,9 +4,13 @@ DIR=${PWD}
 # the configure must be run once beforehand
 cd "$1"
 
+LINKER_OPTS=""
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+  LINKER_OPTS="--linkopt -latomic"
+fi
 # build benchmark binary
 # assuming the configure has set up the NDK and SDK correctly
-bazel build -c opt --linkopt -latomic\
+bazel build -c opt ${LINKER_OPTS} \
   tensorflow/lite/tools/benchmark:benchmark_model
 
 cd ${DIR}
