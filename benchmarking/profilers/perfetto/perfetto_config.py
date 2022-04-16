4a66ef30834ede7fd99bedcaefc0bce4956664c1
Karl Simonsen <karlsi@fb.com>
2022-04-15 17:04:24 -0700

    Fixes for Perfetto battery profiling
    Summary:
    Fixes for Perfetto battery profiling
    
    Update to the latest perfetto spec. for cleaner results (fixes a bug).
    
    Also fixes some bad formatting.
    
    Differential Revision: D35631190
    
    fbshipit-source-id: 2a327006edbaf668a099bcca9b355c261d05f3a3

--- benchmarking/profilers/perfetto/perfetto_config.py
+++ benchmarking/profilers/perfetto/perfetto_config.py
@@ -5,7 +5,7 @@
 # duration_ms: {duration_ms}
 # max_file_size_bytes: 10000000000
 
-CONFIG_TEMPLATE = """
+PERFETTO_CONFIG_TEMPLATE = """\
 buffers: {{
     size_kb: {buffer_size_kb}
     fill_policy: RING_BUFFER
@@ -14,12 +14,12 @@ buffers: {{
     size_kb: {buffer_size2_kb}
     fill_policy: RING_BUFFER
 }}
-{power_config}
-{heapprofd_config}
-{linux_process_stats_config}
-{linux_ftrace_config}
-{android_log_config}
-{track_event_config}
+{power_config}\
+{heapprofd_config}\
+{linux_process_stats_config}\
+{linux_ftrace_config}\
+{android_log_config}\
+{track_event_config}\
 write_into_file: true
 file_write_period_ms: 2500
 max_file_size_bytes: {max_file_size_bytes}
@@ -29,7 +29,7 @@ incremental_state_config {{
 }}
 """
 
-POWER_CONFIG = """
+POWER_CONFIG = """\
 data_sources: {{
     config {{
         name: "android.power"
@@ -44,7 +44,7 @@ data_sources: {{
 }}
 """
 
-HEAPPROFD_CONFIG = """
+HEAPPROFD_CONFIG = """\
 data_sources: {{
     config {{
         name: "android.heapprofd"
@@ -54,13 +54,13 @@ data_sources: {{
             process_cmdline: "{app_name}"
             shmem_size_bytes: {shmem_size_bytes}
             block_client: true
-            {all_heaps}
+{all_heaps_config}\
         }}
     }}
 }}
 """
 
-ANDROID_LOG_CONFIG = """
+ANDROID_LOG_CONFIG = """\
 data_sources: {
     config {
         name: "android.log"
@@ -77,42 +77,42 @@ data_sources: {
     }
 }
 """
-LINUX_PROCESS_STATS_CONFIG = """
-data_sources: {{
-    config {{
+
+LINUX_PROCESS_STATS_CONFIG = """\
+data_sources: {
+    config {
         name: "linux.process_stats"
         target_buffer: 0
-        process_stats_config {{
+        process_stats_config {
             scan_all_processes_on_start: true
             proc_stats_poll_ms: 1000
-        }}
-    }}
-}}
+        }
+    }
+}
 """
 
-LINUX_FTRACE_CONFIG = """
+LINUX_FTRACE_CONFIG = """\
 data_sources: {{
     config {{
         name: "linux.ftrace"
         ftrace_config {{
-            ftrace_events: "sched/sched_switch"
-            ftrace_events: "sched/sched_wakeup_new"
-            ftrace_events: "sched/sched_waking"
-            ftrace_events: "power/cpu_frequency"
-            ftrace_events: "power/cpu_idle"
-            ftrace_events: "power/suspend_resume"
-
             atrace_apps: "{app_name}"
+            ftrace_events: "regulator/regulator_set_voltage"
+            ftrace_events: "regulator/regulator_set_voltage_complete"
+            ftrace_events: "power/clock_enable"
+            ftrace_events: "power/clock_disable"
+            ftrace_events: "power/clock_set_rate"
+            ftrace_events: "power/suspend_resume"
         }}
     }}
 }}
 """
 
-TRACK_EVENT_CONFIG = """
-data_sources: {{
-    config {{
+TRACK_EVENT_CONFIG = """\
+data_sources: {
+    config {
         name: "track_event"
         target_buffer: 1
-    }}
-}}
+    }
+}
 """
