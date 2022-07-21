19efa42ec9feba59e0e8d31211001b36a3601c33
Mark Anderson <andersonmark@fb.com>
2022-07-21 10:05:16 -0700

    Handle 404 and 410 download errors with non-critical exception
    Summary:
    See https://www.internalfb.com/intern/aibench/details/685929341406384
    Benchmarks which reference expired or otherwise non-existant files can be very verbose and generate a critical log and task to oncall.  Adds DownloadNotFoundException to handle 404 and 410 errors without critical logging.
    
    Differential Revision: D38029506
    
    fbshipit-source-id: 7149666026f72cc6e285c7f4c62de81d18c447f5

--- benchmarking/utils/utilities.py
+++ benchmarking/utils/utilities.py
@@ -51,6 +51,10 @@ class DownloadException(BenchmarkException):
 
     pass
 
+class DownloadNotFoundException(BenchmarkException):
+    """Raised where exception occurs when downloading benchmark files."""
+
+    pass
 
 class BenchmarkArgParseException(BenchmarkException):
     """Raised where benchmark arguments could not be parsed or are invalid."""
