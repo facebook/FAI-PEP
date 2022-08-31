26cbe2d73dae7e5e0635266dd09ea2f9cc80b537
Karl Simonsen <karlsi@fb.com>
2022-08-31 15:08:19 -0700

    Consolidate perfetto, simpleperf, and xctrace manifold upload to use upload_output_files() (#524)
    Summary:
    Pull Request resolved: https://github.com/facebook/FAI-PEP/pull/524
    
    Consolidate perfetto, simpleperf, and xctrace output upload code to use upload_output_files().
    
    Reviewed By: axitkhurana
    
    Differential Revision: D38882690
    
    fbshipit-source-id: 4999dd374791fd619686f2021cc77e426a77490a

--- benchmarking/profilers/utilities.py
+++ benchmarking/profilers/utilities.py
@@ -9,44 +9,25 @@
 ##############################################################################
 
 import os
-from typing import Dict, Mapping
+from typing import Dict, Mapping, Optional
 from uuid import uuid4
 
 from bridge.file_storage.upload_files.file_uploader import FileUploader
 from utils.custom_logger import getLogger
 
 
-def generate_perf_filename(model_name="benchmark", hash=None):
+def generate_perf_filename(
+    model_name: Optional[str] = "benchmark", hash: Optional[str] = None
+) -> str:
     """Given the provided model name and optional hash, generate a unique base filename."""
-    unique_name = os.getenv("JOB_IDENTIFIER", None)
+    unique_name: Optional[str] = os.getenv("JOB_IDENTIFIER", None)
     if unique_name is None:
-        unique_name = uuid4()
+        unique_name = f"{uuid4()}"
     elif hash is None:
         hash = os.getenv("JOB_ID", None)
     if hash is not None:
         unique_name += f"_{hash}"
     return f"{model_name}_perf_{unique_name}"
-
-
-def upload_profiling_reports(files: Mapping[str, str]) -> Dict:
-    """
-    Upload to aibench profiling reports.
-    Accepts dict of key -> local file path, uploads using file basename
-    and returns meta dict of key -> url.
-    """
-    meta = {}
-    profiling_reports_uploader = FileUploader("profiling_reports").get_uploader()
-    for key, file in files.items():
-        if not os.path.isfile(file):
-            raise FileNotFoundError(f"File {file} does not exist.")
-        try:
-            url = profiling_reports_uploader.upload_file(file)
-            meta.update({key: url})
-        except Exception as e:
-            getLogger().exception(
-                f"Warning: could not upload {key}: {file}. Skipping.\nException: {e}"
-            )
-    return meta
 
 
 def upload_output_files(files: Mapping[str, str]) -> Dict:
