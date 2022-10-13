4e750b3333547cdf50a0c69d2cb795b6d7fbd0d4
Akshit Khurana <axit@meta.com>
2022-10-13 14:14:30 -0700

    Battery state shouldn't crash AIBench (#531)
    Summary:
    Pull Request resolved: https://github.com/facebook/FAI-PEP/pull/531
    
    Crashes if we can't detect battery status
    
    Reviewed By: KarlSimonsen, vmpuri
    
    Differential Revision: D40357636
    
    fbshipit-source-id: b65ccc97b64a8d555c896ca2726f97dfea9c3fa1

--- benchmarking/platforms/battery_state.py
+++ benchmarking/platforms/battery_state.py
@@ -15,23 +15,28 @@ from typing import Any, Dict
 from platforms.android.adb import ADB
 from platforms.ios.idb import IDB
 
+from utils.custom_logger import getLogger
+
 
 def getBatteryState(device, platform: str, android_dir: str) -> Dict[str, Any]:
     state: Dict[str, Any] = {"supported": False}
-    if platform.startswith("ios"):
-        util = IDB(device)
-        return state  # NYI
-    elif platform.startswith("android"):
-        util = ADB(device, android_dir)
-        if util.isRootedDevice():
-            if not util.user_is_root():
-                util.root(silent=True)
-            state["supported"] = util.getBatteryProp("present") == "1"
+    try:
+        if platform.startswith("ios"):
+            util = IDB(device)
+            return state  # NYI
+        elif platform.startswith("android"):
+            util = ADB(device, android_dir)
+            if util.isRootedDevice():
+                if not util.user_is_root():
+                    util.root(silent=True)
+                state["supported"] = util.getBatteryProp("present") == "1"
 
-        if state["supported"]:
-            state["disconnected"] = util.getBatteryProp("input_suspend") == "1"
-            state["status"] = util.getBatteryProp("status")
-            state["charge_level"] = int(util.getBatteryProp("capacity"))
-            state["temperature"] = int(util.getBatteryProp("temp"))
+            if state["supported"]:
+                state["disconnected"] = util.getBatteryProp("input_suspend") == "1"
+                state["status"] = util.getBatteryProp("status")
+                state["charge_level"] = int(util.getBatteryProp("capacity"))
+                state["temperature"] = int(util.getBatteryProp("temp"))
+    except Exception:
+        getLogger().exception("Failed to get battery state")
 
     return state
